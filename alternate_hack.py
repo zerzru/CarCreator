851e33fe3a3e5edd22a26c75db221aa8f7ad6654
Elisey Sharov <30829308+ZerZru@users.noreply.github.com>
2018-04-05 21:19:00 +0300

    The 'Revolution Update' avialable!

--- alternate_hack.py
+++ alternate_hack.py
@@ -1,17 +1,8 @@
 import random
 import time
 
-path_version = '0.1'
-
 def server_hack():
-	rtp = int(0, 2)
-	if rtp == 0:
-		server_password = 'PzgY3PqhP59$XZ2a?sihhFozdnNM428OC6bQBXIj4xFmY'
-	elif rtp == 1:
-		server_password = 'SFowBvvhRETxsqKpqC4NBF6YMaKGA2xtb1pqKgFWrVZg33'
-	elif rtp == 2:
-		server_password = 'lPSFvDQBMCwS14GGQ3EcymTP3lfWcoz0LTh2$sElVjdbPgpDJuBVI'
-
+	server_password = 'PzgY3PqhP59$XZ2a?sihhFozdnNM428OC6bQBXIj4xFmY'
 	pass_answer = input('Input server password: ')
 	rt = random.randint(0, 10)
 	c_1 = 'BMW'
@@ -32,7 +23,7 @@ def server_hack():
 			pass_answer = input('Input server password: ')
 			if pass_answer == server_password:
 				print('Hacking carried out.')
-				print('A competitor will no longer influence your company.')
+				print('A competitor {} will no longer influence your company.'.format(c_1))
 				print('Did they find your break-in? We will know in 10 seconds....')
 			elif pass_answer != server_password:
 				print('The server is too secure. If you try to hack the server for the 4th time, your hacking attempt will be detected with a 100% probability.')
@@ -40,10 +31,10 @@ def server_hack():
 				print('Did they find your break-in? We will know in 10 seconds...')
 		elif pass_answer == server_password:
 			print('Hacking carried out.')
-			print('A competitor will no longer influence your company.')
+			print('A competitor {} will no longer influence your company.'.format(c_1))
 			print('Did they find your break-in? We will know in 10 seconds....')
 	elif pass_answer == server_password:
 		print('Hacking carried out')
 		print('Did they find your break-in? We will know in 10 seconds....')
-		print('A competitor will no longer influence your company.')
+		print('A competitor {} will no longer influence your company.'.format(c_1))
 	return None
---
+++ carcreator.py
@@ -0,0 +1,307 @@
+try:
+	import json
+	import requests, bs4
+except:
+	print("You don't have requests and BeautifulSoup4 packages. For use all functions of game, please, install this packages")
+
+try:
+	def check_version():
+		s = requests.get('http://scgofficial.esy.es/version.html')
+		b = bs4.BeautifulSoup(s.text, "html.parser")
+		p1 = b.select('.version .p1')
+		result_ver = p1[0].getText()
+		#print(result_ver)
+		version = '003'
+		if result_ver != version:
+			print('You use old version of CarCreator')
+			if version == '001':
+				print('This version will be avialable at 23 March, 2018. Please, update your version for get new functions!')
+		elif result_ver == version:
+			print('You use latest version of CarCreator')
+	check_version()
+except:
+	print("You don't have internet-connection. Check network and try again")
+
+#info
+__author__ = 'Elisey Sharov aka ZerZ™(or ZerZru)'
+version = '002'
+program_language = 'python'
+country = 'Russian Federation'
+license = 'CC-BY-NC'
+path_version = '0.2'
+year = '2018'
+
+en_lang = 'English'
+ru_lang = 'Russian'
+print('If you have a problem, write support()')
+
+try: #try read language settings
+	with open('lang.json', 'r') as fh:
+		data = json.load(fh)
+		answer = data['lang']
+	if answer == en_lang:
+		print('Loaded english language')
+		def input_function():
+			user_function = input('Input function: ')
+			if user_function == 'save':
+				from en_functions import save
+				save()
+				input_function()
+			elif user_function == 'ghelp':
+				from en_functions import ghelp
+				ghelp()
+				input_function()
+			elif user_function == 'gquit':
+				from en_functions import gquit
+				gquit()
+				input_function()
+			elif user_function == 'virus':
+				from en_functions import virus
+				virus()
+				input_function()
+			elif user_function == 'works':
+				from en_functions import works
+				works()
+				input_function()
+			elif user_function == 'clear':
+				from en_functions import clear
+				clear()
+				input_function()
+			elif user_function == 'how_create_a_auto':
+				from en_functions import how_create_a_auto
+				how_create_a_auto()
+				input_function()
+			elif user_function == 'mail':
+				from en_functions import mail
+				mail()
+				input_function()
+			elif user_function == 'support':
+				from en_functions import support
+				support()
+				input_function()
+			elif user_function == 'new_car':
+				from en_functions import new_car
+				new_car()
+				input_function()
+			elif user_function == 'details':
+				from en_functions import details
+				details()
+				input_function()
+			elif user_function == 'how_get_a_money':
+				from en_functions import how_get_a_money
+				how_get_a_money()
+				input_function()
+			elif user_function == 'competitors':
+				from en_functions import competitors
+				competitors()
+				input_function()
+			elif user_function != ghelp or gquit or save or clear or works or virus or how_create_a_auto or mail or support or new_car or details or how_get_a_money or money_help or competitors:
+				print('Function name dont searched. Maybe you use ()')
+				input_function()
+			pass
+		input_function()
+	elif answer == ru_lang:
+		print('Загружен русский язык')
+		def input_function():
+			user_function = input('Введите функцию: ')
+			if user_function == 'save':
+				from ru_functions import save
+				save()
+				input_function()
+			elif user_function == 'ghelp':
+				from ru_functions import ghelp
+				ghelp()
+				input_function()
+			elif user_function == 'gquit':
+				from ru_functions import gquit
+				gquit()
+			elif user_function == 'virus':
+				from ru_functions import virus
+				virus()
+				input_function()
+			elif user_function == 'works':
+				from ru_functions import works
+				works()
+				input_function()
+			elif user_function == 'clear':
+				from ru_functions import clear
+				clear()
+				input_function()
+			elif user_function == 'how_create_a_auto':
+				from ru_functions import how_create_a_auto
+				how_create_a_auto()
+				input_function()
+			elif user_function == 'mail':
+				from ru_functions import mail
+				mail()
+				input_function()
+			elif user_function == 'support':
+				from ru_functions import support
+				support()
+				input_function()
+			elif user_function == 'new_car':
+				from ru_functions import new_car
+				new_car()
+				input_function()
+			elif user_function == 'details':
+				from ru_functions import details
+				details()
+				input_function()
+			elif user_function == 'how_get_a_money':
+				from ru_functions import how_get_a_money
+				how_get_a_money()
+				input_function()
+			elif user_function == 'competitors':
+				from ru_functions import competitors
+				competitors()
+				input_function()
+			elif user_function != ghelp or gquit or save or clear or works or virus or how_create_a_auto or mail or support or new_car or details or how_get_a_money or money_help or competitors:
+				print('Имя функции не найдено. Возможно, вы используете скобки()')
+				input_function()
+			pass
+		input_function()
+except:
+	answer = input('Choose language:\n1) English\n2) Russian\n')
+	if answer == en_lang:
+		dict = {} #write in language settings information about choosen lang
+		dict['lang'] = answer
+		with open('lang.json', mode='w') as f:
+			json.dump(dict, f)
+		print('Loaded english language')
+		def input_function():
+			user_function = input('Input function: ')
+			if user_function == 'save':
+				from en_functions import save
+				save()
+				input_function()
+			elif user_function == 'ghelp':
+				from en_functions import ghelp
+				ghelp()
+				input_function()
+			elif user_function == 'gquit':
+				from en_functions import gquit
+				gquit()
+				input_function()
+			elif user_function == 'virus':
+				from en_functions import virus
+				virus()
+				input_function()
+			elif user_function == 'works':
+				from en_functions import works
+				works()
+				input_function()
+			elif user_function == 'clear':
+				from en_functions import clear
+				clear()
+				input_function()
+			elif user_function == 'how_create_a_auto':
+				from en_functions import how_create_a_auto
+				how_create_a_auto()
+				input_function()
+			elif user_function == 'mail':
+				from en_functions import mail
+				mail()
+				input_function()
+			elif user_function == 'support':
+				from en_functions import support
+				support()
+				input_function()
+			elif user_function == 'new_car':
+				from en_functions import new_car
+				new_car()
+				input_function()
+			elif user_function == 'details':
+				from en_functions import details
+				details()
+				input_function()
+			elif user_function == 'how_get_a_money':
+				from en_functions import how_get_a_money
+				how_get_a_money()
+				input_function()
+			elif user_function == 'competitors':
+				from en_functions import competitors
+				competitors()
+				input_function()
+			elif user_function != ghelp or gquit or save or clear or works or virus or how_create_a_auto or mail or support or new_car or details or how_get_a_money or money_help or competitors:
+				print('Function name dont searched. Maybe you use()')
+				input_function()
+			pass
+		input_function()
+	elif answer == ru_lang:
+		dict = {} #see cooment for en_lang
+		dict['lang'] = answer
+		with open('lang.json', mode='w') as f:
+			json.dump(dict, f)
+		print('Загружен русский язык')
+		def input_function():
+			user_function = input('Введите функцию: ')
+			if user_function == 'save':
+				from ru_functions import save
+				save()
+				input_function()
+			elif user_function == 'ghelp':
+				from ru_functions import ghelp
+				ghelp()
+				input_function()
+			elif user_function == 'gquit':
+				from ru_functions import gquit
+				gquit()
+				input_function()
+			elif user_function == 'virus':
+				from ru_functions import virus
+				virus()
+				input_function()
+			elif user_function == 'works':
+				from ru_functions import works
+				works()
+				input_function()
+			elif user_function == 'clear':
+				from ru_functions import clear
+				clear()
+				input_function()
+			elif user_function == 'how_create_a_auto':
+				from ru_functions import how_create_a_auto
+				how_create_a_auto()
+				input_function()
+			elif user_function == 'mail':
+				from ru_functions import mail
+				mail()
+				input_function()
+			elif user_function == 'support':
+				from ru_functions import support
+				support()
+				input_function()
+			elif user_function == 'new_car':
+				from ru_functions import new_car
+				new_car()
+				input_function()
+			elif user_function == 'details':
+				from ru_functions import details
+				details()
+				input_function()
+			elif user_function == 'how_get_a_money':
+				from ru_functions import how_get_a_money
+				how_get_a_money()
+				input_function()
+			elif user_function == 'competitors':
+				from ru_functions import competitors
+				competitors()
+				input_function()
+			elif user_function != ghelp or gquit or save or clear or works or virus or how_create_a_auto or mail or support or new_car or details or how_get_a_money or money_help or competitors:
+				print('Имя функции не найдено. Возможно, вы используете скобки()')
+				input_function()
+			pass
+		input_function()
+
+def mail():
+	print('Our mail for support, questions and suggestions: scg-publicmail@yandex.ru')
+	pass
+
+def support():
+	print('If you have a technical problem, some function does not work or you have another question or suggestion, you can write to us at mail(enter mail() function) or use this form. If this form does not suit you, then visit scgofficial.esy.es/CarCreator/#support.')
+	a1 = input('Enter your question. Please provide any information that may help us resolve your issue. It is desirable to send log file to e-mail and screenshots of the error. So: ')
+	f = open('question.txt', 'w')
+	f.write(a1)
+	f.close()
+	print('Your question is recorded in the file question.txt. Please send it to our mail. You can find it by writing a mail()')
+	pass
